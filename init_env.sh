7cad3165ee5f16332e8086f7c9533be8bccf2c46
Sriram Viswanathan <sriram.vish@gmail.com>
2022-08-30 21:16:59 -0400

    Initialize env script

--- init_env.sh
+++ init_env.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 source ~/.bashrc
 module load anaconda3
+eval "$(conda shell.bash hook)"
 conda activate /ocean/projects/hum160002p/gsell/.conda/envs/my_env;
