205bd429aa608e438c7dbba0ed7834a7455e3f9b
Sriram Viswanathan <sriram.vish@gmail.com>
2022-08-30 21:29:32 -0400

    Update script to initialize environment

--- init_env.sh
+++ init_env.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-source ~/.bashrc
-eval "$(conda shell.bash hook)"
 module load anaconda3
 conda activate /ocean/projects/hum160002p/gsell/.conda/envs/my_env;
+poetry update
