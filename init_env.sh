b09e2f552d2b3f052593bd96317a03bc7291d189
Sriram Viswanathan <sriram.vish@gmail.com>
2022-08-30 21:11:40 -0400

    Revert renaming - run using poetry

--- init_env.sh
+++ init_env.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-source ~/.bashrc; module load anaconda3; conda activate /ocean/projects/hum160002p/gsell/.conda/envs/my_env;
+source ~/.bashrc
+module load anaconda3
+conda activate /ocean/projects/hum160002p/gsell/.conda/envs/my_env;
 poetry update
