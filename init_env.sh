445ea32d33f6039574cf7d19cc828eb8af7cb3d7
Sriram Viswanathan <sriram.vish@gmail.com>
2022-09-19 20:59:56 -0400

    Check env before creating it

--- init_env.sh
+++ init_env.sh
@@ -2,6 +2,9 @@
 
 export PATH=/ocean/projects/hum160002p/shared/books/code/printprob-env/python/3.9.12/bin:${PATH}
 module load anaconda3
-conda create --name env python=3.9.12 netcdf4 -y
+CONDA_ENV_CREATED=$(conda info -e  | grep ".conda/envs/env")
+if [ $retVal -ne 0 ]; then
+  conda create --name env python=3.9.12 netcdf4 -y
+fi
 conda activate env
 poetry install
