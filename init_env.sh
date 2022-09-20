785d1b298860af6c3a354612044127dc092253fa
Sriram Viswanathan <sriram.vish@gmail.com>
2022-09-19 21:18:17 -0400

    Init

--- init_env.sh
+++ init_env.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 export PATH=/ocean/projects/hum160002p/shared/books/code/printprob-env/python/3.9.12/bin:${PATH}
+echo "Loading anaconda3 module..."
 module load anaconda3
-CONDA_ENV_CREATED=$(conda info -e  | grep ".conda/envs/env")
-if [ "${CONDA_ENV_CREATED}" != "0" ]; then
+OUT=$(conda info --envs  | grep ".conda/envs/env")
+retVal=$?
+if [ $retVal != 0 ]; then
+  echo "Conda env not found, creating one."
   conda create --name env python=3.9.12 netcdf4 -y
 fi
+echo "Activating Conda Env.."
 conda activate env
+echo "Activated."
+echo "Installing Poetry Dependencies..."
 poetry install
+echo "Completed env setup."
