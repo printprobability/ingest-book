08dc0a64d1cd476170edab87d5d94a05dfc512e4
Sriram Viswanathan <sriram.vish@gmail.com>
2022-09-19 21:01:35 -0400

    Check env before creating it

--- init_env.sh
+++ init_env.sh
@@ -3,7 +3,7 @@
 export PATH=/ocean/projects/hum160002p/shared/books/code/printprob-env/python/3.9.12/bin:${PATH}
 module load anaconda3
 CONDA_ENV_CREATED=$(conda info -e  | grep ".conda/envs/env")
-if [ $retVal -ne 0 ]; then
+if [ $retVal != 0 ]; then
   conda create --name env python=3.9.12 netcdf4 -y
 fi
 conda activate env
