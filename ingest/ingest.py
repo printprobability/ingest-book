f06c59f56c12b8bb90e6549260eab1cf75d6eb69
Sriram Viswanathan <sriram.vish@gmail.com>
2022-09-24 15:01:08 -0400

    Update script

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -92,7 +92,7 @@ def _existing_book_for_uuid(uuid):
     try:
         r = requests.get(f'{BOOKS_API_URL}{uuid}/', headers=headers, verify=verify)
         result = r.json().get('results')
-        print("Finding book with UUID: ", uuid)
+        print("Finding book ", f'{BOOKS_API_URL}{uuid}/')
         print("Book result: ", result)
         if result is None or len(result) == 0:
             return None
