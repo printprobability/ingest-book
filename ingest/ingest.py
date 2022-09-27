613776c640085e9c79ec4094cebfed5c64ef87a7
Sriram Viswanathan <sriram.vish@gmail.com>
2022-09-27 17:15:20 -0400

    Make pre-existing UUID none when UUID returned from sheet is empty

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -217,6 +217,7 @@ def run_command(book_string, preexisting_uuid, printer, update):
         # UUID of existing book for the ESTC that we are trying to update or overwrite
         # We should have this UUID in our sheet
         preexisting_uuid = get_uuid_for_book_string(book_string)
+        print("Existing UUID from Google sheet - ", preexisting_uuid)
         if preexisting_uuid and not preexisting_uuid.strip():
             preexisting_uuid = None
 
